62d24010c525667aae84288d85bd7473ee0e9c4a
uhKayla <hey@kayla.lol>
2024-01-29 01:10:56 -0500

    Re-enable UI

--- Editor/AW_AAPSEditor.cs
+++ Editor/AW_AAPSEditor.cs
@@ -1,40 +1,40 @@
-﻿// #if UNITY_EDITOR
-// using System;
-// using System.Collections.Generic;
-// using UnityEditor;
-// using UnityEngine;
-// using VRC.SDKBase;
-// using UnityEngine.UIElements;
-// using UnityEditor.UIElements;
-//
-// namespace ANGELWARE.AW_AAPS.Editor
-// {
-//     [CustomEditor(typeof(AW_AAPS))]
-//     public class AW_MergeDbtsEditor : AW_BaseInspector, IEditorOnly
-//     {
-//         private SerializedProperty _apsMarkers;
-//         private SerializedProperty _oneFloat;
-//
-//
-//         private void OnEnable()
-//         {
-//             _apsMarkers = serializedObject.FindProperty("holeMarkers");
-//             _oneFloat = serializedObject.FindProperty("oneFloatParameter");
-//         }
-//
-//         protected override void SetupContent(VisualElement root)
-//         {
-//             var container = root.Q<VisualElement>("Container");
-//             
-//             var of = new PropertyField(_oneFloat);
-//             container.Add(of);
-//
-//             var s = new PropertyField(_apsMarkers);
-//             s.style.paddingLeft = 7;
-//             container.Add(s);
-//
-//         }
-//     }
-// }
-//
-// #endif
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDKBase;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace ANGELWARE.AW_AAPS.Editor
+{
+    [CustomEditor(typeof(AW_AAPS))]
+    public class AW_MergeDbtsEditor : AW_BaseInspector, IEditorOnly
+    {
+        private SerializedProperty _apsMarkers;
+        private SerializedProperty _oneFloat;
+
+
+        private void OnEnable()
+        {
+            _apsMarkers = serializedObject.FindProperty("holeMarkers");
+            _oneFloat = serializedObject.FindProperty("oneFloatParameter");
+        }
+
+        protected override void SetupContent(VisualElement root)
+        {
+            var container = root.Q<VisualElement>("Container");
+            
+            var of = new PropertyField(_oneFloat);
+            container.Add(of);
+
+            var s = new PropertyField(_apsMarkers);
+            s.style.paddingLeft = 7;
+            container.Add(s);
+
+        }
+    }
+}
+
+#endif
